bd13bbd73fbebb34314f31a6109c0305a4211a44
Patrick Chilton <chpatrick@gmail.com>
2015-06-12 00:04:02 +0100

    Minimization.

--- Clang.hs
+++ Clang.hs
@@ -1,5 +1,3 @@
 module Clang(module C) where
 
 import Clang.FFI as C
-import Clang.Nodes as C
-import Clang.Types as C
--- cbits/util.h
+++
@@ -1,7 +0,0 @@
-typedef void (*haskell_visitor)(CXCursor*);
-
-// Traverse children using a haskell_visitor passed in as client_data.
-static enum CXChildVisitResult visit_haskell(CXCursor cursor, CXCursor parent, CXClientData client_data) {
-  ((haskell_visitor) client_data)(&cursor);
-  return CXChildVisit_Continue;
-};
